07ca40f8702794324669585e48fd5babf7d88f86
Kaichao Sun <kaichaosuna@gmail.com>
2019-11-21 23:44:44 +0800

    fix oracle

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -27,6 +27,9 @@ use version::RuntimeVersion;
 #[cfg(feature = "std")]
 use version::NativeVersion;
 
+use crate::oracle::sr25519::AuthorityId as OracleId;
+use system::offchain::TransactionSubmitter;
+
 // A few exports that help ease life for downstream crates.
 #[cfg(any(feature = "std", test))]
 pub use sr_primitives::BuildStorage;
@@ -63,6 +66,8 @@ pub type DigestItem = generic::DigestItem<Hash>;
 
 /// Used for the module template in `./template.rs`
 mod template;
+
+mod oracle;
 
 /// Opaque types. These are used by the CLI to instantiate machinery that don't need to know
 /// the specifics of the runtime. They can then be made to be agnostic over specific formats
@@ -241,6 +246,16 @@ impl template::Trait for Runtime {
 	type Event = Event;
 }
 
+/// We need to define the Transaction signer for that using the Key definition
+type SubmitTransaction = TransactionSubmitter<OracleId, Runtime, UncheckedExtrinsic>;
+
+impl oracle::Trait for Runtime {
+    type Call = Call;
+    type Event = Event;
+    type AuthorityId = OracleId;
+    type SubmitTransaction = SubmitTransaction;
+}
+
 construct_runtime!(
 	pub enum Runtime where
 		Block = Block,
@@ -258,6 +273,7 @@ construct_runtime!(
 		// Used for the module template in `./template.rs`
 		TemplateModule: template::{Module, Call, Storage, Event<T>},
 		RandomnessCollectiveFlip: randomness_collective_flip::{Module, Call, Storage},
+		Oracle: oracle::{Module, Storage, Call, Event<T>, ValidateUnsigned},
 	}
 );
 
